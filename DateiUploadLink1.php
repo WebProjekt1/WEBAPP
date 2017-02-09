ae9b25762d4c2163cde694b5562bb9cac4377e80
ChristinaMichelle <ck137@hdm-stuttgart.de>
2017-02-09 12:20:07 +0100

    Add files via upload

---
+++ DateiUploadLink1.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chrissy
+ * Date: 08.01.17
+ * Time: 17:16
+ */
+echo
+"<td>
+<input type=\"button\" name=\"change\" value=\"Neues Dokument zu YourBox hinzufügen\" onClick=\"self.location.href='DateiUploadLink2.php'\"></td>";
+?>
---
+++ DateiUploadLink2.php
@@ -0,0 +1,13 @@
+
+<html>
+<body>
+
+<form action="Upload.php" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="sent" value="1">
+    <h2> Wähle ein Dokument zum hochladen: </h2>
+    <input type="file" name="Datei" id="Datei">
+    <input type="submit" value= "Hochladen" name="submit">
+    <input type="Reset" value"Zurücksetzen">
+</form>
+</body>
+</html>
